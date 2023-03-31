29b0d2d5f74d98585c23992d80f72118e405bb33
Laoshin <Laoshin@LaoShin>
2023-03-31 15:01:54 -0300

    enviando para o git o novo projeto via IDE eclipse

---
+++ novo.projeto/src/curso/javaExecutavel/NovaClasseJava.java
@@ -0,0 +1,12 @@
+package curso.javaExecutavel;
+
+public class NovaClasseJava {
+	
+	/*comentário codigo, main metodo executavel java*/
+	public static void main(String[] args) {
+		
+		/*escreve texto no console*/
+		System.out.println("primeira tarefa feita em java do curso Jdev");
+		System.out.println("Teste pratico de print do java curso");
+	}
+}
